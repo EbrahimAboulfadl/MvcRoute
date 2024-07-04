4ea44267795ce6262294d78bcca43fc3841dbaad
Ebrahim Aboulfadl <ebrahimaboulfadl@gmail.com>
2024-07-05 01:21:14 +0300

    adding projects refernces

---
+++ MvcRoute.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcRoute.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
