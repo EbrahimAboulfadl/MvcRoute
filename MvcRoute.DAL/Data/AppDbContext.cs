626457bf404cfee90019dfe6a0409944cb8317b2
Ebrahim Aboulfadl <ebrahimaboulfadl@gmail.com>
2024-07-05 01:17:52 +0300

    Adding Files From The Last Project

---
+++ MvcRoute.DAL/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcRoute.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteMvcProject.DAL.Data
+{
+
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=RouteMvcProject;Integrated Security=True;Trust Server Certificate=True");
+        
+    }
+}
